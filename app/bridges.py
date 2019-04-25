57dc95af752744e9cc2045cc954f7c2063c21f13
Obsidienne <obsiagae@gmail.com>
2019-04-25 20:43:29 +0200

    bridges - ao3_list - handle works & bookmarks
    old: ao3_search - handle works

--- app/bridges.py
+++ app/bridges.py
@@ -17,16 +17,6 @@ class Bridge:
     reverse : bool = False
 
 table = Table(
-    ao3_search = Bridge(
-        match   = lambda url: "archiveofourown.org/works?" in url,
-        rootxp  = "//ol[@class = 'work index group']/li",
-        idxp    = "dl/dd[@class = 'chapters']/text()",
-        titlexp = "div/h4/a/text()",
-        urlxp   = "div/h4/a/@href",
-        descxp  = ".",
-        datexp  = "div/p[@class = 'datetime']/text()",
-        datefmt = "%d %b %Y",
-    ),
     ao3_work = Bridge(
         match   = lambda url: re.search(
             pattern = re.escape("archiveofourown.org/works/")
@@ -40,5 +30,18 @@ table = Table(
         urlxp   = "a/@href",
         datexp  = "span[@class = 'datetime']/text()",
         datefmt = "(%Y-%m-%d)",
+    ),
+    ao3_list = Bridge(
+        match   = lambda url: (
+            "archiveofourown.org/works" in url
+            or "https://archiveofourown.org/bookmarks" in url
+        ),
+        rootxp  = "//ol[contains(@class, 'index group')]/li",
+        idxp    = "dl/dd[@class = 'chapters']/text()",
+        titlexp = "div/h4/a/text()",
+        urlxp   = "div/h4/a/@href",
+        descxp  = ".",
+        datexp  = "div/p[@class = 'datetime']/text()",
+        datefmt = "%d %b %Y",
     ),
 )
